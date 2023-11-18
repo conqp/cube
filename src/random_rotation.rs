47fc27355967e5fc785a005af582908eade88e35
Richard Neumann <mail@richard-neumann.de>
2023-11-18 06:20:20 +0100

    Let cube spin more randomly

--- src/random_rotation.rs
+++ src/random_rotation.rs
@@ -17,6 +17,10 @@ impl RandomRotation {
             rng: thread_rng(),
         }
     }
+
+    fn random(&mut self) -> f64 {
+        (self.rng.gen::<f64>() - 0.1) / 10.0
+    }
 }
 
 impl Default for RandomRotation {
@@ -29,11 +33,11 @@ impl Iterator for RandomRotation {
     type Item = Vec3d;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.position.0 += self.rng.gen::<f64>() / 10.0;
+        self.position.0 += self.random();
         self.position.0 %= TAU;
-        self.position.1 += self.rng.gen::<f64>() / 10.0;
+        self.position.1 += self.random();
         self.position.1 %= TAU;
-        self.position.2 += self.rng.gen::<f64>() / 10.0;
+        self.position.2 += self.random();
         self.position.2 %= TAU;
         Some(self.position)
     }
