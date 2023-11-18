34f79d995254e294388f7a8f3123719246d82d9f
Richard Neumann <mail@richard-neumann.de>
2023-11-18 05:35:20 +0100

    Remove magic numbers

--- src/cube.rs
+++ src/cube.rs
@@ -1,3 +1,6 @@
+const DEFAULT_SIZE: u8 = 20;
+const DEFAULT_SIDES: [char; 6] = ['@', '$', '~', '#', ';', '+'];
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Cube {
     size: u8,
@@ -23,6 +26,6 @@ impl Cube {
 
 impl Default for Cube {
     fn default() -> Self {
-        Self::new(20, ['@', '$', '~', '#', ';', '+'])
+        Self::new(DEFAULT_SIZE, DEFAULT_SIDES)
     }
 }
